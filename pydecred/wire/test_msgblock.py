28523eb8ac20ac2fc38add0147d03b670cce44e3
Nicola 'tekNico' Larosa <github@teknico.net>
2019-12-05 18:20:58 -0500

    Reformat the pydecred/wire/ directory.
    Disable reformatting a few sections to keep inline comments aligned.

--- pydecred/wire/test_msgblock.py
+++ pydecred/wire/test_msgblock.py
@@ -4,12 +4,20 @@ See LICENSE for details
 """
 
 import unittest
-from tinydecred.pydecred.wire import msgblock
+
 from tinydecred.crypto.bytearray import ByteArray
+from tinydecred.pydecred.wire import msgblock
+
 
 class TestBlockHeader(unittest.TestCase):
     def test_decode(self):
-        encoded = "060000000bd25508e99bf6f8399efce65762b55873d69dd05a7871631ac8fa7a36f1d05c977ea75040b905415cbc8f7dd519831a031ef5cd9c6a187a9eab8136c8b44fda000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000ffff7f20204e0000000000001b00000066010000aadd025d00000000255221163779dfe800000000000000000000000000000000000000000000000000000000"
+        encoded = (
+            "060000000bd25508e99bf6f8399efce65762b55873d69dd05a7871631ac8fa7a36f1d05c"
+            "977ea75040b905415cbc8f7dd519831a031ef5cd9c6a187a9eab8136c8b44fda00000000"
+            "000000000000000000000000000000000000000000000000000000000100000000000000"
+            "0000000000000000ffff7f20204e0000000000001b00000066010000aadd025d00000000"
+            "255221163779dfe800000000000000000000000000000000000000000000000000000000"
+        )
         header = msgblock.BlockHeader.deserialize(ByteArray(encoded))
         print("version: %s" % repr(header.version))
         print("prevBlock: %s" % repr(reversed(header.prevBlock).hex()))
@@ -31,4 +39,7 @@ class TestBlockHeader(unittest.TestCase):
         print("stakeVersion: %s" % repr(header.stakeVersion))
         recoded = header.serialize().hex()
         self.assertEqual(recoded, encoded)
-        self.assertEqual(header.id(), "52dc18bd18910e0e785411305b04f1281353ab29135a144c0fca9ea4746c2b66")
+        self.assertEqual(
+            header.id(),
+            "52dc18bd18910e0e785411305b04f1281353ab29135a144c0fca9ea4746c2b66",
+        )
--- pydecred/wire/wire.py
+++ pydecred/wire/wire.py
@@ -5,20 +5,22 @@ See LICENSE for details
 
 Constants and common routines from the dcrd wire package.
 """
+
 from tinydecred.crypto.bytearray import ByteArray
 
-MaxInt8   = (1<<7) - 1
+# fmt: off
+MaxInt8   = (1 << 7) - 1
 MinInt8   = -1 << 7
-MaxInt16  = (1<<15) - 1
+MaxInt16  = (1 << 15) - 1
 MinInt16  = -1 << 15
-MaxInt32  = (1<<31) - 1
+MaxInt32  = (1 << 31) - 1
 MinInt32  = -1 << 31
-MaxInt64  = (1<<63) - 1
+MaxInt64  = (1 << 63) - 1
 MinInt64  = -1 << 63
-MaxUint8  = (1<<8) - 1
-MaxUint16 = (1<<16) - 1
-MaxUint32 = (1<<32) - 1
-MaxUint64 = (1<<64) - 1
+MaxUint8  = (1 << 8) - 1
+MaxUint16 = (1 << 16) - 1
+MaxUint32 = (1 << 32) - 1
+MaxUint64 = (1 << 64) - 1
 
 # Commands used in message headers which describe the type of message.
 CmdVersion        = "version"
@@ -47,17 +49,18 @@ CmdGetCFTypes     = "getcftypes"
 CmdCFilter        = "cfilter"
 CmdCFHeaders      = "cfheaders"
 CmdCFTypes        = "cftypes"
+# fmt: on
 
 # MaxMessagePayload is the maximum bytes a message can be regardless of other
 # individual limits imposed by messages themselves.
-MaxMessagePayload = (1024 * 1024 * 32) # 32MB
+MaxMessagePayload = 1024 * 1024 * 32  # 32MB
 
 # MaxBlockPayloadV3 is the maximum bytes a block message can be in bytes as of
 # version 3 of the protocol.
-MaxBlockPayloadV3 = 1000000 # Not actually 1MB which would be 1024 * 1024
+MaxBlockPayloadV3 = 1000000  # Not actually 1MB which would be 1024 * 1024
 
 # MaxBlockPayload is the maximum bytes a block message can be in bytes.
-MaxBlockPayload = 1310720 # 1.25MB
+MaxBlockPayload = 1310720  # 1.25MB
 
 # ProtocolVersion is the latest protocol version this package supports.
 ProtocolVersion = 6
@@ -84,12 +87,13 @@ TxSerializeOnlyWitness = 2
 # extended Decred script.
 DefaultPkScriptVersion = 0x0000
 
+
 def varIntSerializeSize(i):
     """
     The value is small enough to be represented by itself, so it's
     just 1 byte.
     """
-    if i < 0xfd:
+    if i < 0xFD:
         return 1
 
     # Discriminant 1 byte plus 2 bytes for the uint16.
@@ -103,30 +107,32 @@ def varIntSerializeSize(i):
     # Discriminant 1 byte plus 8 bytes for the uint64.
     return 9
 
+
 def writeVarInt(pver, val):
     """
     writeVarInt serializes val to w using a variable number of bytes depending
     on its value.
     """
 
-    if val < 0xfd:
-        return ByteArray(val, length=1) # will be length 1
+    if val < 0xFD:
+        return ByteArray(val, length=1)  # will be length 1
 
     if val <= MaxUint16:
-        b = ByteArray(0xfd)
+        b = ByteArray(0xFD)
         b += ByteArray(val, length=2).littleEndian()
         return b
 
     if val <= MaxUint32:
-        b = ByteArray(0xfe)
+        b = ByteArray(0xFE)
         b += ByteArray(val, length=4).littleEndian()
         return b
 
-    b = ByteArray(0xff)
+    b = ByteArray(0xFF)
     b += ByteArray(val, length=8).littleEndian()
     return b
 
-def writeVarBytes(pver, inBytes): #w io.Writer, pver uint32, bytes []byte) error {
+
+def writeVarBytes(pver, inBytes):  # w io.Writer, pver uint32, bytes []byte) error {
     """
     writeVarBytes serializes a variable length byte array to w as a varInt
     containing the number of bytes, followed by the bytes themselves.
@@ -136,37 +142,44 @@ def writeVarBytes(pver, inBytes): #w io.Writer, pver uint32, bytes []byte) error
     b += inBytes
     return b
 
-def readVarInt(b, pver): #r io.Reader, pver uint32) (uint64, error) {
+
+def readVarInt(b, pver):  # r io.Reader, pver uint32) (uint64, error) {
     """
     readVarInt reads a variable length integer from r and returns it as a uint64.
     """
     discriminant = b.pop(1).int()
     rv = 0
-    if discriminant == 0xff:
+    if discriminant == 0xFF:
         rv = b.pop(8).unLittle().int()
 
         # The encoding is not canonical if the value could have been
         # encoded using fewer bytes.
         minRv = 0x100000000
         if rv < minRv:
-            raise Exception("ReadVarInt noncanon error: %d - %d <= %d" % (rv, discriminant, minRv))
-    elif discriminant == 0xfe:
+            raise Exception(
+                "ReadVarInt noncanon error: %d - %d <= %d" % (rv, discriminant, minRv)
+            )
+    elif discriminant == 0xFE:
         rv = b.pop(4).unLittle().int()
 
         # The encoding is not canonical if the value could have been
         # encoded using fewer bytes.
         minRv = 0x10000
         if rv < minRv:
-            raise Exception("ReadVarInt noncanon error: %d - %d <= %d" % (rv, discriminant, minRv))
+            raise Exception(
+                "ReadVarInt noncanon error: %d - %d <= %d" % (rv, discriminant, minRv)
+            )
 
-    elif discriminant == 0xfd:
+    elif discriminant == 0xFD:
         rv = b.pop(2).unLittle().int()
 
         # The encoding is not canonical if the value could have been
         # encoded using fewer bytes.
-        minRv = 0xfd
+        minRv = 0xFD
         if rv < minRv:
-            raise Exception("ReadVarInt noncanon error: %d - %d <= %d" % (rv, discriminant, minRv))
+            raise Exception(
+                "ReadVarInt noncanon error: %d - %d <= %d" % (rv, discriminant, minRv)
+            )
 
     else:
         rv = discriminant
