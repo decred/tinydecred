507a5f5b84179dc9aeef8e842d4fd5aa04976644
Nicola Larosa <github@teknico.net>
2020-02-14 13:22:00 +0100

    tests: NAF and splitK in crypto.secp256k1.curve (#74)
    * Convert ./tests/unit/crypto/secp256k1/test_curve.py to pytest format.
    
    * Add NAF tests to curve.py . Also move randBytes to util.helpers.
    
    * Added tests for curve.splitK, fixed a double bug in it.
    
    * Run black and isort.
    
    * Use a dictionary instead of dataclasses to keep supporting Python 3.6.
    
    * Remove the redundant dataclasses import too.
    
    * Changes per review:
    
    - test helpers "randBytes" and "sign" removed from util/helpers.py and moved
      to a conftest.py file in the tests/ root directory, as pytest fixtures;
    - test_curve.py changed to use the new fixtures;
    - unit/util/test_database.py restored to its original state.

---
+++ decred/tests/conftest.py
@@ -0,0 +1,36 @@
+"""
+Copyright (c) 2019, the Decred developers
+See LICENSE for details
+"""
+
+import random
+
+import pytest
+
+
+@pytest.fixture
+def sign():
+    def _sign(x):
+        """
+        x: number
+        """
+        if x < 0:
+            return -1
+        elif x > 0:
+            return 1
+        else:
+            return 0
+
+    return _sign
+
+
+# Seed initialization is delegated to tests.
+# random.seed(0)
+
+
+@pytest.fixture
+def randBytes():
+    def _randBytes(low=0, high=50):
+        return bytes(random.randint(0, 255) for _ in range(random.randint(low, high)))
+
+    return _randBytes
