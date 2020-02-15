aaaf9a111187c57e88b6328a68aaca46f15bc93c
Nicola Larosa <github@teknico.net>
2020-02-15 17:37:12 +0100

    tests: convert database unit tests to the pytest format (#75)
    * Add NAF tests to curve.py . Also move randBytes to util.helpers.
    
    * Added tests for curve.splitK, fixed a double bug in it.
    
    * Remove the redundant dataclasses import too.
    
    * Changes per review:
    
    - test helpers "randBytes" and "sign" removed from util/helpers.py and moved
      to a conftest.py file in the tests/ root directory, as pytest fixtures;
    - test_curve.py changed to use the new fixtures;
    - unit/util/test_database.py restored to its original state.
    
    * Convert decred/tests/unit/util/test_database.py to the pytest format.
    
    This allows using the randBytes fixture and avoiding its redefinition.
    Also add a prepareLogger fixture with module scope.
    
    * Initialize the random seed.
    
    * Fix rebase.

--- decred/tests/unit/util/test_database.py
+++ decred/tests/unit/util/test_database.py
@@ -6,19 +6,14 @@ See LICENSE for details
 import os.path
 import random
 from tempfile import TemporaryDirectory
-import unittest
 
-from decred.util import database, helpers
+import pytest
+
+from decred.util import database
 from decred.util.encode import ByteArray
 
 
-random.seed(0)
-randInt = random.randint
-randByte = lambda: randInt(0, 255)
-
-
-def randBytes(low=0, high=50):
-    return bytes(randByte() for _ in range(randInt(low, high)))
+LOGGER_ID = "TestDB"
 
 
 class TBlobber:
@@ -38,130 +33,129 @@ class TBlobber:
         return self.b == other.b
 
 
-class TestDB(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        helpers.prepareLogger("TestDB")
+def test_database(prepareLogger, randBytes):
+    with TemporaryDirectory() as tempDir:
 
-    def test_database(self):
-        with TemporaryDirectory() as tempDir:
+        # Open a key value db in the temp directory.
+        master = database.KeyValueDatabase(os.path.join(tempDir, "tmp.sqlite"))
 
-            # Open a key value db in the temp directory.
-            master = database.KeyValueDatabase(os.path.join(tempDir, "tmp.sqlite"))
+        # '$' in bucket name is illegal.
+        with pytest.raises(ValueError):
+            master.child("a$b")
 
-            # '$' in bucket name is illegal.
-            self.assertRaises(ValueError, lambda: master.child("a$b"))
+        try:
+            db = master.child("test")
 
-            try:
-                db = master.child("test")
+            # Again, '$' in bucket name is illegal.
+            with pytest.raises(ValueError):
+                db.child("c$d")
 
-                # '$' in bucket name is illegal.
-                self.assertRaises(ValueError, lambda: db.child("c$d"))
+            # Create some test data.
+            random.seed(0)
+            testPairs = [(randBytes(low=1), randBytes()) for _ in range(20)]
+            runPairs(db, testPairs)
 
-                # Create some test data.
-                testPairs = [(randBytes(low=1), randBytes()) for _ in range(20)]
-                self.runPairs(db, testPairs)
+            # check integer keys and child naming scheme
+            intdb = db.child("inttest", datatypes=("INTEGER", "BLOB"))
+            assert intdb.name == "test$inttest"
+            intdb[5] = b"asdf"
+            assert intdb[5] == b"asdf"
 
-                # check integer keys and child naming scheme
-                intdb = db.child("inttest", datatypes=("INTEGER", "BLOB"))
-                self.assertEqual(intdb.name, "test$inttest")
-                intdb[5] = b"asdf"
-                self.assertEqual(intdb[5], b"asdf")
+            # check uniqueness of keys:
+            k = testPairs[0][0]
+            db[k] = b"some new bytes"
+            assert len([key for key in db if key == k]) == 1
 
-                # check uniqueness of keys:
-                k = testPairs[0][0]
-                db[k] = b"some new bytes"
-                self.assertEqual(len([key for key in db if key == k]), 1)
+            # test a serializable object
+            randBlobber = lambda: TBlobber(ByteArray(randBytes()))
+            objDB = master.child("blobber", blobber=TBlobber, unique=False)
+            testPairs = [(randBytes(low=1), randBlobber()) for _ in range(20)]
+            runPairs(objDB, testPairs)
 
-                # test a serializable object
-                randBlobber = lambda: TBlobber(ByteArray(randBytes()))
-                objDB = master.child("blobber", blobber=TBlobber, unique=False)
-                testPairs = [(randBytes(low=1), randBlobber()) for _ in range(20)]
-                self.runPairs(objDB, testPairs)
+            # non-uniqueness of keys
+            k = testPairs[0][0]
+            objDB[k] = randBlobber()
+            assert len([key for key in objDB if key == k]) == 2
 
-                # non-uniqueness of keys
-                k = testPairs[0][0]
-                objDB[k] = randBlobber()
-                self.assertEqual(len([key for key in objDB if key == k]), 2)
+            # test a second-level child
+            kidDB = db.child("kid")
+            testPairs = [(randBytes(low=1), randBytes()) for _ in range(20)]
+            runPairs(kidDB, testPairs)
 
-                # test a second-level child
-                kidDB = db.child("kid")
-                testPairs = [(randBytes(low=1), randBytes()) for _ in range(20)]
-                self.runPairs(kidDB, testPairs)
+            # uniqueness of table keys
+            k = testPairs[0][0]
+            kidDB[k] = b"some new bytes"
+            assert len([key for key in kidDB if key == k]) == 1
 
-                # uniqueness of table keys
-                k = testPairs[0][0]
-                kidDB[k] = b"some new bytes"
-                self.assertEqual(len([key for key in kidDB if key == k]), 1)
+        finally:
+            master.close()
 
-            finally:
-                master.close()
 
-    def runPairs(self, db, testPairs):
-        ogKeys = {k: v for k, v in testPairs}
-        values = [v for _, v in testPairs]
+def runPairs(db, testPairs):
+    ogKeys = {k: v for k, v in testPairs}
+    values = [v for _, v in testPairs]
 
-        # Ensure the db has zero length.
-        self.assertTrue(len(db) == 0)
+    # Ensure the db has zero length.
+    assert len(db) == 0
 
-        # Insert the test pairs.
-        for k, v in testPairs:
-            db[k] = v
+    # Insert the test pairs.
+    for k, v in testPairs:
+        db[k] = v
 
-        # Check length again
-        self.assertEqual(len(db), len(testPairs))
+    # Check length again
+    assert len(db) == len(testPairs)
 
-        # Check items iteration
-        for k, v in db.items():
-            self.assertIn(k, ogKeys)
-            self.assertEqual(v, ogKeys[k])
+    # Check items iteration
+    for k, v in db.items():
+        assert k in ogKeys
+        assert v == ogKeys[k]
 
-        # Check key iteration.
-        for k in db:
-            self.assertIn(k, ogKeys)
-            del ogKeys[k]
-        self.assertEqual(len(ogKeys), 0)
+    # Check key iteration.
+    for k in db:
+        assert k in ogKeys
+        del ogKeys[k]
+    assert len(ogKeys) == 0
 
-        # Check value iteration.
-        for v in db.values():
-            values.remove(v)
-        self.assertEqual(len(values), 0)
+    # Check value iteration.
+    for v in db.values():
+        values.remove(v)
+    assert len(values) == 0
 
-        # Delete an item
-        k = testPairs[0][0]
+    # Delete an item
+    k = testPairs[0][0]
+    del db[k]
+    # Check the length again
+    assert len(db) == len(testPairs) - 1
+
+    # Make sure the right row was deleted.
+    with pytest.raises(database.NoValue):
+        v = db[k]
+
+    # Remmove the corresponding test pair from the dict.
+    testPairs.pop(0)
+
+    # Make sure the rest are retrievable.
+    for k, _ in testPairs:
+        v = db[k]
+
+    # Delete the rest
+    for k, v in testPairs:
         del db[k]
-        # Check the length again
-        self.assertEqual(len(db), len(testPairs) - 1)
 
-        # Make sure the right row was deleted.
-        with self.assertRaises(database.NoValue):
-            v = db[k]
+    # Check the length
+    assert len(db) == 0
 
-        # Remmove the corresponding test pair from the dict.
-        testPairs.pop(0)
+    # Insert again
+    for k, v in testPairs:
+        db[k] = v
 
-        # Make sure the rest are retrievable.
-        for k, _ in testPairs:
-            v = db[k]
+    # Make sure nothing has changed.
+    for k, _ in testPairs:
+        assert k in db
 
-        # Delete the rest
-        for k, v in testPairs:
-            del db[k]
-
-        # Check the length
-        self.assertEqual(len(db), 0)
-
-        # Insert again
-        for k, v in testPairs:
-            db[k] = v
-
-        # Make sure nothing has changed.
-        for k, _ in testPairs:
-            self.assertTrue(k in db)
-
-        # Clear the database, batch insert, and try again.
-        db.clear()
-        self.assertEqual(len(db), 0)
-        db.batchInsert(testPairs)
-        for k, _ in testPairs:
-            self.assertTrue(k in db)
+    # Clear the database, batch insert, and try again.
+    db.clear()
+    assert len(db) == 0
+    db.batchInsert(testPairs)
+    for k, _ in testPairs:
+        assert k in db
