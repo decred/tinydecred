46d190996802cae0a0993f21574b2ad9a67064cf
buck54321 <buck54321@gmail.com>
2020-03-18 09:08:59 -0500

    add account discovery
    Adds an account discovery method to AccountManager that can be used
    on wallets restored from seed.

--- decred/tests/unit/wallet/test_accounts.py
+++ decred/tests/unit/wallet/test_accounts.py
@@ -8,7 +8,7 @@ import pytest
 from decred import DecredError
 from decred.crypto import crypto, rando
 from decred.dcr import nets
-from decred.util import database, encode
+from decred.util import chains, database, encode
 from decred.wallet import accounts
 
 
@@ -44,8 +44,10 @@ def test_change_addresses(prepareLogger):
     cryptoKey = rando.newKey()
     db = database.KeyValueDatabase(":memory:").child("tmp")
     # ticker for coin type is ok. Case insensitive.
-    am = accounts.createNewAccountManager(tRoot, cryptoKey, "DcR", nets.mainnet, db)
-    acct = am.openAccount(0, cryptoKey)
+    acctMgr = accounts.createNewAccountManager(
+        tRoot, cryptoKey, "DcR", nets.mainnet, db
+    )
+    acct = acctMgr.openAccount(0, cryptoKey)
     for i in range(10):
         acct.nextInternalAddress()
 
@@ -54,31 +56,62 @@ def test_account_manager(prepareLogger):
     cryptoKey = rando.newKey()
     db = database.KeyValueDatabase(":memory:").child("tmp")
     # 42 = Decred
-    am = accounts.createNewAccountManager(tRoot, cryptoKey, 42, nets.mainnet, db)
+    acctMgr = accounts.createNewAccountManager(tRoot, cryptoKey, 42, nets.mainnet, db)
 
-    acct = am.openAccount(0, cryptoKey)
-    tempAcct = am.addAccount(cryptoKey, "temp")
-    assert am.account(1) == tempAcct
-    assert am.listAccounts() == [acct, tempAcct]
-    am.accounts[3] = tempAcct
-    del am.accounts[1]
+    acct = acctMgr.openAccount(0, cryptoKey)
+    tempAcct = acctMgr.addAccount(cryptoKey, "temp")
+    assert acctMgr.account(1) == tempAcct
+    assert acctMgr.listAccounts() == [acct, tempAcct]
+    acctMgr.accounts[3] = tempAcct
+    del acctMgr.accounts[1]
     with pytest.raises(AssertionError):
-        am.listAccounts()
-    del am.accounts[3]
+        acctMgr.listAccounts()
+    del acctMgr.accounts[3]
 
     with pytest.raises(DecredError):
         accounts.AccountManager.unblob(encode.BuildyBytes(0))
 
     zeroth = acct.currentAddress()
-    b = am.serialize()
+    b = acctMgr.serialize()
     reAM = accounts.AccountManager.unblob(b.b)
 
-    assert am.coinType == reAM.coinType
-    assert am.netName == reAM.netName
-    assert am.netName == reAM.netName
+    assert acctMgr.coinType == reAM.coinType
+    assert acctMgr.netName == reAM.netName
+    assert acctMgr.netName == reAM.netName
 
     reAM.load(db, None)
     reAcct = reAM.openAccount(0, cryptoKey)
     reZeroth = reAcct.currentAddress()
 
     assert zeroth == reZeroth
+
+
+def test_discover(prepareLogger):
+    cryptoKey = rando.newKey()
+    db = database.KeyValueDatabase(":memory:").child("tmp")
+    acctMgr = accounts.createNewAccountManager(
+        tRoot, cryptoKey, "dcr", nets.mainnet, db
+    )
+    txs = {}
+
+    class Blockchain:
+        params = nets.mainnet
+        addrsHaveTxs = lambda addrs: any(a in txs for a in addrs)
+
+    ogChain = chains.chain("dcr")
+    chains.registerChain("dcr", Blockchain)
+    ogLimit = accounts.ACCOUNT_GAP_LIMIT
+    accounts.ACCOUNT_GAP_LIMIT = 2
+
+    acctMgr.discover(cryptoKey)
+    assert len(acctMgr.accounts) == 1
+
+    coinExtKey = acctMgr.coinKey(cryptoKey)
+    acct2ExtKey = coinExtKey.deriveAccountKey(2).neuter().child(0)
+    acct2Addr5 = acct2ExtKey.deriveChildAddress(5, nets.mainnet)
+    txs[acct2Addr5] = ["tx"]
+    acctMgr.discover(cryptoKey)
+    assert len(acctMgr.accounts) == 3
+
+    chains.registerChain("dcr", ogChain)
+    accounts.ACCOUNT_GAP_LIMIT = ogLimit
