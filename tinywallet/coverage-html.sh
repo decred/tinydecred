b97636708278ebab5bc726611669c3fdaaad3f85
Nicola 'tekNico' Larosa <github@teknico.net>
2020-04-23 12:17:53 -0400

    tinywallet tests: add coverage infra, complete config coverage.

---
+++ tinywallet/coverage-html.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+## To generate line-by-line test coverage viewable in a web browser,
+## change to this directory, then install the dependencies:
+# poetry install
+## The coverage report will be in the ./htmlcov/ directory.
+poetry run pytest --cov-report=html --cov=tinywallet ./tests/
